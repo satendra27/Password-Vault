2e39a44b801450336a2704d13de7de3751618a75
satendra27 <143244442+satendra27@users.noreply.github.com>
2023-09-16 20:43:21 +0530

    vault.py

--- vault.py
+++ vault.py
@@ -1,65 +1,80 @@
 from tkinter import *
+import tkinter as tk
+import openpyxl,xlrd
+from openpyxl import Workbook
+import pathlib
 from tkinter import messagebox
 
-def save():
-    website=website_entry.get()
-    email=email_entry.get()
-    password=password_entry.get()
-    if len(website)==0 or len(password)==0:
-        messagebox.showerror(title="Ooops",message="Don't Leave any field Empty!")
-    else:
-        is_ok=messagebox.askokcancel(title=website,message=f"You have Entered\nEmail: {email}\nPassword: {password}\n"f"Are You Sure to Save?")
-        if is_ok:
-            with open("data.txt","a")as f:
-                f.write(f"Your Application Name or Website:{website} |E-mail: {email} |Password: {password}\n")
-                website_entry.delete(0,END)
-                password_entry.delete(0,END)
-                email_entry.delete(0,END)
-                
-def show():
-    if password_entry.cget('show')=='*':
-        password_entry.config(show='')
-    else:
-        password_entry.config(show='*')    
-                        
 root=Tk()
 root.title("Password Vault")
 root.geometry("800x600")
 root.resizable(0,0)
-root.config(padx=50,pady=50,background="skyblue")
-# canvas=Canvas(width=100,height=100)
-# photo=PhotoImage(file="icons8-lock-50.png")
-# canvas.create_image(50,50,image=photo,)
-# canvas.grid(column=1,row=0)
+
+
+file=pathlib.Path("Passwords.xlsx")
+if file.exists():
+    pass
+else:
+    file=Workbook()
+    sheet=file.active
+    sheet["A1"]="Application Name"
+    sheet["B1"]="Application URL"
+    sheet["C1"]="Password"
+    
+    file.save("Passwords.xlsx")
+    
+def show():
+    if password_entry.cget('show')=='*':
+        password_entry.config(show='')
+    else:
+        password_entry.config(show='*')   
+        
+def submit():
+    y=website_entry.get()
+    z=email_entry.get()
+    z1=password_entry.get()
+    print(y)
+    print(z)
+    print(z1)
+    
+    file=openpyxl.load_workbook("Passwords.xlsx")
+    sheet=file.active
+    sheet.cell(column=1,row=sheet.max_row+1,value=y)
+    sheet.cell(column=2,row=sheet.max_row,value=z)
+    sheet.cell(column=3,row=sheet.max_row,value=z1)
+    
+    file.save("Passwords.xlsx")
+    messagebox.showinfo("Password Vault","Your Data Have been Successfully Saved...")
+    
+    
+frame=LabelFrame(root,text='Password Details',background="skyblue").pack(expand='yes',fill='both')
+
+
 img=PhotoImage(file="output-onlinepngtools.png")
-label_img=Label(root,image=img,background="skyblue")
-label_img.grid(column=1,row=0)
+label_img=Label(frame,image=img,background="skyblue")
+label_img.place(x=300,y=100)
 
-label=Label(root,text="Password Vault",font=("Imprint MT shadow",25),background="skyblue")
-label.grid(column=1,row=1,pady=10)
+label=Label(frame,text="Password Vault",font=("Imprint MT shadow",25),background="skyblue")
+label.place(x=300,y=300)
 
-website=Label(text="Application Name or Website : ",background="skyblue",font=("cambria",13))
-website.grid(column=0,row=2,pady=5,padx=5)
-email=Label(text="Email/Username : ",background="skyblue",font=("cambria",13))
-email.grid(column=0,row=3,pady=5,padx=50)
-password=Label(text="Password : ",background="skyblue",font=("cambria",13))
-password.grid(column=0,row=4,pady=5,padx=50)
+website=Label(frame,text="Application Name or Website : ",background="skyblue",font=("cambria",13))
+website.place(x=100,y=360)
+email=Label(frame,text="Email/Username : ",background="skyblue",font=("cambria",13))
+email.place(x=150,y=400)
+password=Label(frame,text="Password : ",background="skyblue",font=("cambria",13))
+password.place(x=150,y=440)
 
-website_entry=Entry(width=28,font=("arial",15),bd=2,highlightcolor="Blue",highlightthickness=2)
-website_entry.grid(column=1,row=2,columnspan=2,pady=5)
-email_entry=Entry(width=28,font=("arial",15),bd=2,highlightcolor="Blue",highlightthickness=2)
-email_entry.grid(column=1,row=3,columnspan=2,pady=5)
-#email_entry.insert(0,"baghelsatendra@gmail.com")
-password_entry=Entry(width=22,show='*',font=("arial",15),bd=2,highlightcolor="Blue",highlightthickness=2)
-password_entry.grid(column=1,row=4,pady=5)
+website_entry=Entry(frame,width=28,font=("arial",15),bd=2,highlightcolor="Blue",highlightthickness=2)
+website_entry.place(x=350,y=360)
+email_entry=Entry(frame,width=28,font=("arial",15),bd=2,highlightcolor="Blue",highlightthickness=2)
+email_entry.place(x=350,y=400)
+password_entry=Entry(frame,width=22,show='*',font=("arial",15),bd=2,highlightcolor="Blue",highlightthickness=2)
+password_entry.place(x=350,y=440)
 
 
-generate_btn=Button(text="Show",command=show,activebackground="green",bd=3,highlightcolor="Blue",highlightthickness=2)
-generate_btn.grid(column=2,row=4,pady=5)
-add_btn=Button(text="Add",width=36,command=save,background="lightgreen",font=("arial",10),activebackground="lightblue",bd=3)
-add_btn.grid(column=1,row=5,columnspan=2,pady=15)
-
-purpose=Label(root,text="Purpose : Password Vault allows users to create unique,\n complex passwords for every online account without needing to remember them.",background="skyblue",fg="red",font=("Bell MT",13))
-purpose.grid(columnspan=4)
+generate_btn=Button(frame,text="Show",activebackground="green",command=show,bd=3,highlightcolor="Blue",highlightthickness=2)
+generate_btn.place(x=612,y=441)
+add_btn=Button(frame,text="Add",width=36,command=submit,font=("arial",10),activebackground="lightblue",bd=3)
+add_btn.place(x=350,y=490)
 
 root.mainloop()
